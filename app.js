577085de4ceb49b896ae9d9373ff8fde7ae1ec62
kemalbekcan <kemal.bekcan@akinon.com>
2023-12-23 20:26:08 +0300

    initial commit

--- app.js
+++ app.js
@@ -1,17 +1,56 @@
 import express from 'express';
+import helmet from 'helmet';
+import path from 'path';
+import fs from 'fs';
+import imagemin from 'imagemin';
+import imageminMozjpeg from 'imagemin-mozjpeg';
+import compression from 'compression'
+import CleanCSS from 'clean-css';
 import cors from 'cors';
 import { fileURLToPath } from 'url';
-import path from 'path';
+
 
 const app = express();
+
+// Helmet middleware'i ekleyin ve CSP özelliklerini özelleştirin
+app.use(helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", 'trusted-scripts.com'],
+    },
+  }));
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(__dirname + '/public'));
+// CSS sıkıştırma işlemi
+const cssPath = path.join(__dirname, 'public', 'styles', 'style.css');
+const minifiedCSSPath = path.join(__dirname, 'public', 'styles', 'style.min.css');
+
+const cssContent = fs.readFileSync(cssPath, 'utf8');
+const minifiedCSSContent = new CleanCSS().minify(cssContent).styles;
+
+fs.writeFileSync(minifiedCSSPath, minifiedCSSContent);
+
+
+// Resim sıkıştırma işlemi
+const inputImagePath = path.join(__dirname, '/public', 'images');
+const outputImagePath = path.join(__dirname, '/public', 'compressed-images');
+
+imagemin([`${inputImagePath}/*.{jpg,png}`], {
+    destination: outputImagePath,
+    plugins: [imageminMozjpeg({ quality: 80 })],
+}).then(() => {
+    console.log('Images compressed successfully!');
+});
+
+// Gzip sıkıştırma middleware'ini ekleyin
+app.use(compression());
+
+app.use(express.static(path.join(__dirname, '/public')));
 app.use(cors());
 app.use(express.json());
- 
+
 app.set('view engine', 'ejs');
 
 // index page 
@@ -38,7 +77,7 @@ app.get('/', function (req, res) {
 });
 
 // about page
-app.get('/about', function(req, res) {
+app.get('/about', function (req, res) {
     res.render('pages/about');
 });
 
