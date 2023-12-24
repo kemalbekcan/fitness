0e04fcdedb340feb75fde6502cb46b9c01f97e4b
kemalbekcan <kemal.bekcan@akinon.com>
2023-12-25 00:00:22 +0300

    initial commit

--- build-scss.js
+++ build-scss.js
@@ -1,6 +1,5 @@
 // build-scss.js
 
-import { exec } from 'child_process';
 import postcss from 'postcss';
 import autoprefixer from 'autoprefixer';
 import fs from 'fs';
@@ -26,13 +25,13 @@ sass.render(
                 .then((result) => {
                     const minifiedCSSContent = new CleanCSS().minify(result.css).styles;
                     fs.writeFileSync(outputFilePath, minifiedCSSContent);
-                    console.log('SCSS compiled, Autoprefixer applied, and CSS minified successfully!');
+                    console.log(chalk.green('SCSS compiled, Autoprefixer applied, and CSS minified successfully!'));
                 })
                 .catch((error) => {
-                    console.error('Error applying Autoprefixer:', error);
+                    console.log(chalk.red('Error applying Autoprefixer:', error));
                 });
         } else {
-            console.error('Error compiling SCSS:', error);
+            console.log(chalk.red('Error compiling SCSS:', error));
         }
     }
 );
@@ -41,4 +40,4 @@ sass.render(
 const jsContent = fs.readFileSync(jsFilePath, 'utf8');
 const minifiedJSContent = UglifyJS.minify(jsContent).code;
 fs.writeFileSync(minifiedJSPath, minifiedJSContent);
-console.log('JS minified successfully!');
+console.log(chalk.green('JS minified successfully!'));
